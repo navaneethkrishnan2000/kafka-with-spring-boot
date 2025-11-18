6984182411b548d0cecfb709b4acf91adc8f38cd
Navaneeth <navaneethkrishnans2000@gmail.com>
2025-11-18 11:20:02 +0530

    Added consumer to the project

---
+++ library-events-consumer/src/main/java/com/example/library_events_consumer/LibraryEventsConsumerApplication.java
@@ -0,0 +1,13 @@
+package com.example.library_events_consumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryEventsConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryEventsConsumerApplication.class, args);
+	}
+
+}
